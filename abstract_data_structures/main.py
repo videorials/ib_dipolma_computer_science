9d27d9340a8528e60a2dbd772f0f7259d04063c9
bramwax <andy_bramwell@hotmail.com>
2023-04-10 08:05:12 +0200

    added addtwonumbers

--- abstract_data_structures/main.py
+++ abstract_data_structures/main.py
@@ -7,12 +7,12 @@ def main():
 	# linked lists representing two positive integers in reverse order
     a1, a2 = [2,4,3], [5,6,4]
     l1, l2 = LinkedList(), LinkedList()
-    for i in a1: l1.add_first(i)
-    for i in a2: l2.add_first(i)
+    for i in a1: l1.addFirst(i)
+    for i in a2: l2.addFirst(i)
 
     solution = Solution()
     output = solution.add_two_numbers(l1,l2)
-    output.print_list()
+    output.list()
 	
 
 if __name__ == "__main__":
